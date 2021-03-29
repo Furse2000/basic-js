9c0e9b42b2ba88809ddef990c098cd75288fa110
Furse2000 <61042829+Furse2000@users.noreply.github.com>
2021-03-30 00:32:42 +0300

    feat:improved

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,45 +1,11 @@
-
 const CustomError = require("../extensions/custom-error");
-
-module.exports = function createDreamTeam(memb) {
- let temp='';
- let n=228;
- 
-  for(let i=0;i<memb.length;i++)
-  {
-    if((typeof memb[i] === "string" || memb[i] instanceof String)==0)
-    {
-      memb.splice(i,1);
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+  const teamName = [];
+  members.forEach(member => {
+    if (typeof member === 'string') {
+      teamName.push(member.trim().split('')[0].toUpperCase());
     }
-    for(let j=0;j<memb[i].length;j++)
-    {
-      if(memb[i][j]==' ')
-      { temp=memb[i];
-        temp.splice(j,1);
-        memb[i]=temp;
-        j--;
-      }
-    }
-    memb[i]=memb[i][0];
-    
-  }
-  /*for(let i=0;i<memb.length;i++)
-  {
-    for(let j=0;j<memb[i].length;j++)
-    {if(j=0)
-      memb[i][j]=memb[i][j];
-      
-    }
-    break;
-  }
-*/
-  if(memb.sort()!='')
-  {
- return memb.join('').toUpperCase();
- //temp=memb.String; 
-  //return temp.toUpperCase();
-  }
-  else return false
-  
-};
-
+  });
+  return teamName.sort().join('');
+}
