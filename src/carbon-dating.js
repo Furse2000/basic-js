7a11f3cbeb5140108797269350b2ea3708e31930
Furse2000 <61042829+Furse2000@users.noreply.github.com>
2021-03-30 00:14:56 +0300

    feat: remove trash

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,9 +4,7 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  //throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-  // npm run test ./test/carbon-dating.test.js
+ 
   if (Number.isNaN(Number(sampleActivity))) {
     return false;
 }
