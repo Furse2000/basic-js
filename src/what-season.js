7d9723a691c6d356b487738b48e6ff81876057c8
Furse2000 <61042829+Furse2000@users.noreply.github.com>
2021-03-30 00:31:27 +0300

    feat:improved

--- src/what-season.js
+++ src/what-season.js
@@ -1,32 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(data) {
-  if(data==0)
-  return null;
-if(data.getMonth()>11)
-{
-  return 'Error';
-}
-else
-{
-  if(data.getMonth()<(-1))
-  {
-    return 'Error';
-  }
-  else
-  {
-    if(data.getMonth()==0||data.getMonth()==1||data.getMonth()==11)
-    return 'winter';
-    if(data.getMonth()==2||data.getMonth()==3||data.getMonth()==4)
-    return 'spring';
-    if(data.getMonth()==5||data.getMonth()==6||data.getMonth()==7)
-    return 'summer';
-    if(data.getMonth()==8||data.getMonth()==8||data.getMonth()==10)
-    return 'autumn';
-  }
-  
+module.exports = function getSeason(date) {
+  if (!date) return 'Unable to determine the time of year!'; 
 
-}
+  if (!(date instanceof Date)) throw new Error();
 
-// remove line with error and write your code here
-};
+  const seasons = {
+    spring: [2, 3, 4],
+    summer: [5, 6, 7],
+    autumn: [8, 9, 10],
+    winter: [11, 0, 1]
+  };
+
+  const month = Date.prototype.getMonth.call(date);
+
+  for (const key in seasons) {
+    if (seasons[key].includes(month)) return key;
+  }
+}
